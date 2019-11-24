bd4c789db8894238ffcffee11eec6899d84386af
Kevin Hsu <mkhsu@uw.edu>
2019-11-24 15:54:22 -0800

    extract problem body

--- pushshift.py
+++ pushshift.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import random
+import html
 
 difficulty = 'easy'     # or intermediate or hard
 
@@ -16,8 +17,8 @@ target = data[index]['id']
 
 json_string = requests.get('https://api.pushshift.io/reddit/search/submission/?ids=' + target).text
 
-data = json.JSONDecoder().decode(json_string)['data']
+data = json.JSONDecoder().decode(json_string)['data'][0]
 
-print(data[0])
+print(data['title'] + '\n' + html.unescape(data['selftext']))
 
 # https://github.com/pushshift/api
