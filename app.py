7758210a09c7c70f66ce5e4ab7eb657536bd4b58
Brian Hou <brianghou@gmail.com>
2019-11-29 23:34:03 -0800

    Implemented generating random question and user specified difficulty question.

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import boto3
 import pushshift
 import html
+
 
 
 application = app = Flask(__name__)
@@ -17,7 +18,7 @@ def random():
     data_with_br = data.replace("\n", "<br />")
     title = returned_data['title'] + '\n' 
     title_with_br = title.replace("\n", "<br />")
-    print(data + title)
+    #print(data + title)
 
 
     return render_template("displayproblem.html", context=data_with_br, title=title_with_br)
@@ -27,7 +28,16 @@ def random():
 
 @app.route("/generate", methods=['POST'])
 def generate():
-    return "index.html"
+    difficulty_type = request.form["difficulty"]
+    print(difficulty_type)
+    difficulty_type_data = pushshift.difficulty_specified_problem(difficulty_type)
+    print(difficulty_type_data)
+    returned_difficulty_type_data = html.unescape(difficulty_type_data['selftext'])
+    returned_difficulty_type_data = returned_difficulty_type_data.replace("\n", "<br />")
+    title_type_with_br = difficulty_type_data['title'] + '\n' 
+    title_type_with_br = title_type_with_br.replace("\n", "<br />")
+
+    return render_template("displayproblem.html", context_two=returned_difficulty_type_data, title_two=title_type_with_br)
 
 @app.route("/subscribe", methods=['POST'])
 def subscribe():
--- pushshift.py
+++ pushshift.py
@@ -29,6 +29,26 @@ def return_random_problem():
 
     #return data['title'] + '\n' + html.unescape(data['selftext'])
 
+def difficulty_specified_problem(difficulty_type):
+
+
+    json_string = requests.get(r'https://api.pushshift.io/reddit/search/submission/?subreddit=dailyprogrammer&title=' + difficulty_type + r'%20challenge&size=500').text
+
+    data = json.JSONDecoder().decode(json_string)['data']
+
+    res_len = len(data)
+
+    index = random.randrange(0, res_len)
+
+    target = data[index]['id']
+
+    json_string = requests.get('https://api.pushshift.io/reddit/search/submission/?ids=' + target).text
+
+    data = json.JSONDecoder().decode(json_string)['data'][0]
+    
+    return data
+
+    #return data['title'] + '\n' + html.unescape(data['selftext'])
 
 
 
